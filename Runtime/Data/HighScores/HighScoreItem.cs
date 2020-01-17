d3999938484edf61458fd06708db8f0202a684aa
Pedro Duran <pedrojosuedm@hotmail.com>
2020-01-16 21:16:21 -0400

    remove highscores

--- Runtime/Data/HighScores/HighScoreItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QSocial.Data.HighScores
-{
-    [System.Serializable]
-    public sealed class HighScoreItem
-    {
-        public string UserId;
-        public int ScoreReached;
-    }
-}
