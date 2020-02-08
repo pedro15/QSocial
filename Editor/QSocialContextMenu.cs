d8324b59a17325fd57ac41921f8957d9bb57f0d5
Pedro Duran <pedrojosuedm@hotmail.com>
2020-02-07 23:38:12 -0400

    Moving Import resources menu

--- Editor/QSocialContextMenu.cs
+++ Editor/QSocialContextMenu.cs
@@ -5,47 +5,19 @@ namespace QSocialEditor
 {
     internal class QSocialContextMenu
     {
-        static string GetPackagePath()
-        {
-            string _PACKAGEPATH = Path.GetFullPath("Packages/com.pedro15.qsocial");
+        static string GetPackagePath() => Path.GetFullPath("Packages/com.pedro15.qsocial/MainResources");
 
-            if (Directory.Exists(_PACKAGEPATH))
-            {
-                return _PACKAGEPATH;
-            }
-
-            _PACKAGEPATH = Path.GetFullPath("Packages/QSocial");
-
-            if (Directory.Exists(_PACKAGEPATH))
-            {
-                return _PACKAGEPATH;
-            }
-
-            _PACKAGEPATH = Path.GetFullPath("Assets/..");
-
-            if (Directory.Exists(_PACKAGEPATH))
-            {
-                if (Directory.Exists(_PACKAGEPATH + "/Assets/QSocial"))
-                {
-                    _PACKAGEPATH += "Assets/QSocial";
-                    return _PACKAGEPATH;
-                }
-            }
-
-            return null;
-        }
-
-        [MenuItem("QSocial/Install Facebook Support")]
+        [MenuItem("Tools/QSocial/Install Facebook Support")]
         static void InstallFacebookSupport()
         {
-            string p = GetPackagePath().Replace(@"/", @"\") + @"\MainResources\FBSupport.unitypackage";
+            string p = GetPackagePath() + "/FBSupport.unitypackage";
             AssetDatabase.ImportPackage(p, false);
         }
 
-        [MenuItem("QSocial/Install Essentials")]
+        [MenuItem("Tools/QSocial/Install Essentials")]
         static void InstallEssentials()
         {
-            string p = GetPackagePath().Replace(@"/", @"\") + @"\MainResources\Essentials.unitypackage";
+            string p = GetPackagePath() + "/Essentials.unitypackage";
             AssetDatabase.ImportPackage(p, false);
         }
 
